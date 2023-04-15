9bb14b349622bcc3f0d07e475aee83bf82ebce47
Anurag Phadnis <phadnis.anurag@gmail.com>
2023-04-16 02:59:17 +0530

    fix get api

--- pov-app-backend/src/pov/pov.controller.ts
+++ pov-app-backend/src/pov/pov.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Logger, NotFoundException, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Logger, NotFoundException, Param, Post, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { PovService } from './pov.service';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
@@ -12,11 +12,12 @@ export class PovController {
     constructor(private readonly povService: PovService) {}
 
     @Get()
-    async getPovs(@Param('hash') hash: string) {
+    async getPovs(@Query('hash') hash: string) {
         const pov = await this.povService.getPov(hash);
         if (pov === null || pov === undefined) {
             throw new NotFoundException(`No POV found for hash: ${hash}`);
         }
+		return pov;
     }
 
     @Post()
@@ -25,22 +26,31 @@ export class PovController {
         shouldGivePOV: boolean,
         callbackUrl: string,
         externalId: string,
+		hash: string,
+		lat: number,
+		long: number,
     }) {
 			try {
-				console.log(file);
-				const fileExtName = extname(file.originalname);
-				const randomName = Array(4)
-					.fill(null)
-					.map(() => Math.round(Math.random() * 16).toString(16))
-					.join('');
-					await axios.get(data.callbackUrl, {
-							params: {
-									shouldGivePov: data.shouldGivePOV,
-									externalId: data.externalId,
-							}
-					})
+				// await axios.get(data.callbackUrl, {
+					// 		params: {
+						// 				shouldGivePov: data.shouldGivePOV,
+						// 				externalId: data.externalId,
+						// 		}
+						// })
+						const shouldGivePOV = data.shouldGivePOV;
+					if (shouldGivePOV) {
+						await this.povService.createPov({
+							externalId: data.externalId,
+							hash: data.hash,
+							localtion: {
+								lat: data.lat,
+								long: data.long,
+							},
+							time: new Date(),
+						});
+					}
 					return {
-							pov: data.shouldGivePOV,
+						pov: data.shouldGivePOV,
 					};
 			} catch (error) {
 				Logger.error('Error occured while upload file', error);
--- pov-app-backend/src/pov/pov.service.ts
+++ pov-app-backend/src/pov/pov.service.ts
@@ -5,6 +5,7 @@ import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 import { diskStorage } from 'multer';
 import { InjectModel } from '@nestjs/mongoose';
+import {v4 as uuid} from 'uuid';
 
 @Injectable()
 export class PovService {
@@ -18,6 +19,36 @@ export class PovService {
         });
     }
 
-
+    async createPov(data: {
+        externalId: string,
+        time: Date,
+        localtion: {
+            lat: number,
+            long: number
+        },
+        hash: string,
+    }) {
+        const current = await this.getPov(data.hash);
+        if (current === null) {
+            return await this.proofOfVoteModel.create({
+                hash: data.hash,
+                pov: [{
+                    externalId: data.externalId,
+                    id: uuid(),
+                    localtion: data.localtion,
+                    time: data.time,
+                }]
+            })
+        }
+        current.pov.push({
+            hash: data,
+            pov: [{
+                externalId: data.externalId,
+                id: uuid(),
+                localtion: data.localtion,
+                time: data.time,
+            }]
+        })
+    }
 
 }
